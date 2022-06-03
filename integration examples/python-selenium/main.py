0c06481659134eeac5051812e1be7bff17c26891
Nils Wagener <nils@aqua-cloud.io>
2022-06-03 10:19:27 +0200

    Add python selenium example for integration with the aqua agent

---
+++ integration examples/python-selenium/main.py
@@ -0,0 +1,44 @@
+#!/bin/sh
+"exec" "`dirname $0`/.venv/bin/python" "$0" "$@"
+
+from time import sleep
+import unittest
+import geckodriver_autoinstaller
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+class PythonTestExample(unittest.TestCase):
+
+    def setUp(self) -> None:
+        geckodriver_autoinstaller.install()
+
+        options = webdriver.FirefoxOptions()
+        options.headless = True
+        self.driver = webdriver.Firefox(options=options)
+
+        return super().setUp()
+
+
+    def test_search_in_python_org(self) -> None:
+        self.driver.get("https://www.python.org")
+
+        self.assertIn("Python", self.driver.title)
+
+        elem = self.driver.find_element(By.NAME, "q")
+        elem.clear()
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+
+        self.assertNotIn("No results found.", self.driver.page_source)
+
+
+    def tearDown(self) -> None:
+        self.driver.quit()
+
+        return super().tearDown()
+
+
+if __name__ == "__main__":
+    unittest.main()
